842bf72c83813ab0a84d8d7c6cb4380ae97f88f9
eunbyul <iks1614@gmail.com>
2024-03-14 17:19:37 +0900

    서베이 읽기, 서베이 검사 - 코드정리(static method)

--- src/test/java/org/silverstar/util/FileReaderUtilTest.java
+++ src/test/java/org/silverstar/util/FileReaderUtilTest.java
@@ -9,21 +9,19 @@ import java.io.UncheckedIOException;
 
 class FileReaderUtilTest {
 
-    private final FileReaderUtil fileReaderUtil = new FileReaderUtil();
-
     @Test
     @DisplayName("실패 - readSurveyFile - 파라미터 파일명이 잘못되었을때")
     void fail_readSurveyFile_파라미터_파일명이_잘못되었을때() {
         Assertions.assertThrows(NullPointerException.class, () -> {
-            fileReaderUtil.readSurveyFile(null, null);
+            FileReaderUtil.readSurveyFile(null, null);
         });
 
         Assertions.assertThrows(UncheckedIOException.class, () -> {
-            fileReaderUtil.readSurveyFile("", null);
+            FileReaderUtil.readSurveyFile("", null);
         });
 
         Assertions.assertThrows(NullPointerException.class, () -> {
-            fileReaderUtil.readSurveyFile("test.txt", null);
+            FileReaderUtil.readSurveyFile("test.txt", null);
         });
     }
 
@@ -31,11 +29,11 @@ class FileReaderUtilTest {
     @DisplayName("실패 - readSurveyFile - 파라미터 func이 잘못되었을때")
     void fail_readSurveyFile_파라미터_func이_잘못되었을때() {
         Assertions.assertThrows(NullPointerException.class, () -> {
-            fileReaderUtil.readSurveyFile("survey.csv", null);
+            FileReaderUtil.readSurveyFile("survey.csv", null);
         });
 
         Assertions.assertThrows(RuntimeException.class, () -> {
-            fileReaderUtil.readSurveyFile("survey.csv", (s) -> {
+            FileReaderUtil.readSurveyFile("survey.csv", (s) -> {
                 throw new RuntimeException();
             });
         });
@@ -44,14 +42,14 @@ class FileReaderUtilTest {
     @Test
     @DisplayName("성공 - readSurveyFile")
     void success_readSurveyFile() {
-        fileReaderUtil.readSurveyFile("survey.csv", (s) -> "return string");
+        FileReaderUtil.readSurveyFile("survey.csv", (s) -> "return string");
     }
 
     @Test
     @DisplayName("실패 - validateLine - 파라미터 파일타입이 잘못되었을때")
     void fail_validateLine_파라미터_파일타입이_잘못되었을때() {
         Assertions.assertThrows(NullPointerException.class, () -> {
-            fileReaderUtil.validateLine(null, null);
+            FileReaderUtil.validateLine(null, null);
         });
     }
 
@@ -59,21 +57,21 @@ class FileReaderUtilTest {
     @DisplayName("실패 - validateLine - 파라미터 String 배열이 잘못되었을때")
     void fail_파라미터_String배열이_잘못되었을때() {
         Assertions.assertThrows(NullPointerException.class, () -> {
-            fileReaderUtil.validateLine(FileType.SURVEY, null);
+            FileReaderUtil.validateLine(FileType.SURVEY, null);
         });
 
         Assertions.assertEquals(false,
-            fileReaderUtil.validateLine(FileType.SURVEY, new String [] {}));
+                FileReaderUtil.validateLine(FileType.SURVEY, new String [] {}));
 
         Assertions.assertEquals(false,
-            fileReaderUtil.validateLine(FileType.SURVEY, new String [] {"string"}));
+                FileReaderUtil.validateLine(FileType.SURVEY, new String [] {"string"}));
     }
 
     @Test
     @DisplayName("성공 - validateLine")
     void success_validateLine() {
         Assertions.assertEquals(true,
-            fileReaderUtil.validateLine(FileType.SURVEY, new String [] {"100", "만족도설문조사"}));
+                FileReaderUtil.validateLine(FileType.SURVEY, new String [] {"100", "만족도설문조사"}));
     }
 
 }
