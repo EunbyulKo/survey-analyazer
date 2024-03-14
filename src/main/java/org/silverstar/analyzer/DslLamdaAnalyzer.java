1839d586a0481cc38593113fabf1a304ab8fceb7
eunbyul <iks1614@gmail.com>
2024-03-14 19:04:22 +0900

    서베이 읽기, 서베이 검사 - 코드정리(unused import)

--- src/main/java/org/silverstar/analyzer/DslLamdaAnalyzer.java
+++ src/main/java/org/silverstar/analyzer/DslLamdaAnalyzer.java
@@ -1,13 +1,10 @@
 package org.silverstar.analyzer;
 
-import org.silverstar.model.*;
+import org.silverstar.model.ResourceFromFiles;
 import org.silverstar.service.FileReaderService;
 import org.silverstar.service.ResourceInspectService;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class DslLamdaAnalyzer {
